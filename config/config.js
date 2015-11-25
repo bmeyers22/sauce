074394bb94c1aa05129a45997fec4bce13f2ab4c
Brian Meyers <meyers.briant@gmail.com>
2015-11-24 21:07:23 -0500

    Added production config

--- config/config.js
+++ config/config.js
@@ -51,6 +51,30 @@ var config = {
             name: 'sauce'
         },
         port: 3000,
+        venmo: {
+            sandboxParams: {
+                userId: '145434160922624933',
+                email: 'venmo@venmo.com',
+                phone: '15555555555',
+                note: 'Test payment',
+                paymentAmount: {
+                    settled: '0.10',
+                    failed: '0.20',
+                    pending: '0.30',
+                    settledCharge: '-0.10',
+                    pendingCharge: '-0.20'
+                }
+            },
+            meEndpoint: 'https://api.venmo.com/v1/me',
+            paymentEndpointDev: 'https://sandbox-api.venmo.com/v1/payments',
+            paymentEndpoint: 'https://api.venmo.com/v1/payments',
+            scope: 'access_email, access_phone, access_profile, make_payments',
+            endpoint: 'https://api.venmo.com/v1/oauth/access_token',
+            redirectUri: 'http://localhost:4200/login',
+            clientId: '2929',
+            clientSecret: 'SHTz2rnN5UeWFXm8DR6jUUb9E9BrqTJ3'
+        }
+
     }
 };
 
